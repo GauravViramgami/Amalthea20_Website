863d9ad7a8430452beb6b9a622f51d0d48d039c8
Gaurav Viramgami <58519896+Gaurav7214@users.noreply.github.com>
2020-10-27 09:57:49 +0530

    code

--- routes/index.js
+++ routes/index.js
@@ -55,6 +55,11 @@ router.get("/webinar_form", function (req, res) {
   let ref = req.query.ref;
   if (ref === undefined) {
     ref = -1;
+  } else {
+    ref = ref.toString();
+    if (ref.length === 3 && ref.substring(0,1) === "0" && ref.substring(1,2) != "0") {
+      ref = ref.substring(1,4);
+    }
   }
   res.render("webinar_form", {ref: ref})
 
